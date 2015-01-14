0216546c1bfda63bb55b20ed2a64868a85db6866
qetbn <aleks.omelich@gmail.com>
2015-01-14 15:43:56 +0000

    add simple storage manager pt.2

---
+++ .c9/metadata/preview-https:/gazerjs-qetbn.c9.io
@@ -0,0 +1,1 @@
+{"filter":false,"title":"[P] https://gazerjs-qetbn.c9.io","undoManager":{"mark":0,"position":-1,"stack":[]},"preview":{"path":"https://gazerjs-qetbn.c9.io/player.html","previewer":"preview.browser"},"timestamp":1418980030162,"hash":"da39a3ee5e6b4b0d3255bfef95601890afd80709"}
---
+++ src/gazer.storageManager.js
@@ -0,0 +1,42 @@
+gazer.recorder.storageManager = {
+    apiUrl: "/api.php",
+    lastItem: "",
+    /**
+     * Send whole data to server
+     */
+    sendData: function(data){
+        /**
+         * Remember last sent item
+         */
+        gazer.recorder.storageManager.lastItem = gazer.recorder.frames.length;
+        /**
+         * Send request to API
+         */
+        $.ajax({
+            type: "POST",
+            url: gazer.recorder.storageManager.apiUrl,
+            data: {
+                frames: JSON.stringify(gazer.recorder.frames)
+            },
+            success: gazer.recorder.storageManager.successCallback,
+            error: gazer.recorder.storageManager.errorCallback
+        });
+        return true;
+    },
+    /**
+     * If server recieved data
+     */
+    successCallback: function(){
+        /**
+         * Cut all items till lastItem
+         */
+        gazer.recorder.cutFrames(0,gazer.recorder.storageManager.lastItem);
+        return true;
+    },
+    /**
+     * If there's a problem
+     */
+     errorCallback: function() {
+        return true;   
+     }
+};
